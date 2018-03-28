e6b081f1133a04b2e16c7f8a475d965ec123a35b
00King00 <mr.1bilous@gmail.com>
2018-03-28 22:45:46 +0300

    add js code to advantagelist

--- dev/js/code.js
+++ dev/js/code.js
@@ -1,11 +1,17 @@
 $(function(){
-SameHeight();
-function SameHeight() {
-		var browserMinWidth = $(window).width()
-			if (browserMinWidth > 600) {
-				$('.logo').css('height', $('.trade-4').height());
-			}
-$(window).resize(function(){
 	SameHeight();
-});	
+	function SameHeight() {
+		var browserMinWidth = $(window).width();
+		if (browserMinWidth > 600) {
+			$('.logo').css('height', $('.trade-4').height());
+		}
+		if (browserMinWidth > 800) {
+			$('.advantagelist__try').css('height', $('.advantagelist__wrap-inner-left').height() - 30);
+			
+		}
+		
+	}
+	$(window).resize(function(){
+		SameHeight();
+	});
 });
